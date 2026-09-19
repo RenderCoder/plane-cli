b4d25d65b8b4d8f956468c9cb854671d8e909305
Homon Grok 4.5 <homon@silicon-alchemists.local>
2026-09-19 06:29:15 -0700

    fix: nest platform urls under on_arm/on_intel for brew style
    Homebrew 7 FormulaAudit/ComponentsOrder rejects bare url/sha256 inside
    on_macos/on_linux; nest under on_arm/on_intel blocks.

--- Formula/plane-cli.rb
+++ Formula/plane-cli.rb
@@ -5,18 +5,21 @@ class PlaneCli < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/RenderCoder/plane-cli/releases/download/plane-cli-v0.1.3/plane-cli-darwin-arm64.tar.gz"
       sha256 "4635f2368104c2d5f000d4baebb8c50649885e1af2693976e539901eaf1d5ab1"
-    else
+    end
+    on_intel do
       url "https://github.com/RenderCoder/plane-cli/releases/download/plane-cli-v0.1.3/plane-cli-darwin-x64.tar.gz"
       sha256 "8c190750ea376b5da537b45cfad770e23741549d61c28e4144c84195fb5ee605"
     end
   end
 
   on_linux do
-    url "https://github.com/RenderCoder/plane-cli/releases/download/plane-cli-v0.1.3/plane-cli-linux-x64.tar.gz"
-    sha256 "c8c6bc08ce829efb34ed3a61d3ca4f6f05e20a94fd4dde250c6d77e4e596527d"
+    on_intel do
+      url "https://github.com/RenderCoder/plane-cli/releases/download/plane-cli-v0.1.3/plane-cli-linux-x64.tar.gz"
+      sha256 "c8c6bc08ce829efb34ed3a61d3ca4f6f05e20a94fd4dde250c6d77e4e596527d"
+    end
   end
 
   def install
@@ -27,3 +30,4 @@ class PlaneCli < Formula
     assert_match version.to_s, shell_output("#{bin}/plane-cli --version")
   end
 end
+
